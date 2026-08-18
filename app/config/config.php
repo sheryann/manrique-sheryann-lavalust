b1471567d79ff4510fc734b61b6c84d960d67342
Shery Ann Manrique <sheryannmanrique37@gmail.com>
2026-08-18 19:34:05 +0800

    Update Dockerfile

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['environment'] = getenv('APP_ENV') ?: 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url']= 'http://127.0.0.1/LavaLust/';
+$config['base_url']= 'https://manrique-sheryann-lavalust.onrender.com';
 
 /*
 |--------------------------------------------------------------------------
@@ -99,7 +99,7 @@ $config['proxy_enabled']           = FALSE;
 | variable to blank.
 |
 */
-$config['index_page']               = 'index.php';
+$config['index_page']               = '';
 
 /*
 |--------------------------------------------------------------------------
